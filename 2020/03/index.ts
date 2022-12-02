2ab137bfbd2bd1e93adcfe3f30b26ceea745c3cb
Amine Diboune <mohamedaminediboune@gmail.com>
2022-12-02 19:11:52 +0100

    2020 day 03

--- 2020/03/index.ts
+++ 2020/03/index.ts
@@ -1,35 +1,37 @@
 const input = await Deno.readTextFile("./input.txt")
 
 const lines = input.split(/\n/)
-lines.shift()
 
-17029250784
-351348270
-
-const findTrees = (steps : number, row: number) => {
+const countTrees = (steps, row? = 1) => {
     let trees = 0
-    lines.map((line, index) => {
-        const isOdd = index & 1 ? "odd" : "even"
-        console.log()
-        if (isOdd == "odd" && row !== 1) {
-            const factor = (index + steps) * row
-        
-            const isTree = line.repeat(index + 1)[factor] === "#"
-            if (isTree) {
-                trees = trees + 1
-            }    
+
+    lines.filter((newLine, index) => {
+        if (index % row != 0) {
+            return false
+        } else {
+            return true
         }
-    }
-    )
+    }).map((line, index) => {
+            
+            const longerLine = line.repeat(index)
+            const factor = (index) * steps
+            if (longerLine[factor] === "#") {
+                trees = trees + 1
+            }
+        })
+
     return trees
 }
 
-const route13 = findTrees(1,3)
-const route11 = findTrees(1,1)
-const route51 = findTrees(5,1)
-const route71 = findTrees(7,1)
-const route12 = findTrees(1,2)
 
+const r1d1 = countTrees(1)
+const r3d1 = countTrees(3)
+const r5d1 = countTrees(5)
+const r7d1 = countTrees(7)
+const r1d2 = countTrees(1,2)
 
-console.log("Part 1: The solution is" + " " + route13)
-console.log("Part 2: The solution is" + " " + route13 * route11 * route51 * route71 * route12)
+const s1 = r3d1
+const s2 = r1d1 * r3d1 * r5d1 * r7d1 * r1d2
+
+console.log("Part 1: The solution is" + " " + s1)
+console.log("Part 2: The solution is" + " " + s2 )
